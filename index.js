3e003190eed5585aba2d21034eff184fa87d5b2b
Johan Vargas Sanchez <johanvargasdev@gmail.com>
2023-03-02 17:40:35 -0500

    challenge 3 complete

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const urls = [];
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+  const shortUrl = urls.length + 1;
+
+  const urlObject = new URL(originalUrl);
+  const hostname = urlObject.hostname;
+
+  dns.lookup(hostname, (err, address) => {
+    if (err) {
+      res.json({ error: "invalid url" });
+    } else {
+      urls.push({ originalUrl, shortUrl });
+      res.json({ original_url: originalUrl, short_url: shortUrl });
+    }
+  });
+});
+
+app.get("/api/shorturl/:shortUrl", (req, res) => {
+  const { shortUrl } = req.params;
+  const url = urls.find((u) => u.shortUrl == shortUrl);
+
+  if (url) {
+    res.redirect(url.originalUrl);
+  } else {
+    res.status(404).send("URL not found");
+  }
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
